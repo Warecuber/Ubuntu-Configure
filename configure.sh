558cca130b72dfec0ad816dfe19c9873aa0ba28f
warecuber <warecuber@gmail.com>
2021-02-16 14:45:56 -0600

    Added code to create new user group to allow SSH

--- configure.sh
+++ configure.sh
@@ -3,6 +3,9 @@ echo "===== Starting configuration script ====="
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install htop neofetch glances -y
+echo "===== Creating SSH Group ====="
+sudo groupadd sshuser
+sudo usermod -aG sshuser $USER
 echo "===== Configuring SSH ====="
 sh ./ssh.sh
 sudo ufw enable
