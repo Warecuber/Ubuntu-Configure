0747200c9394e84d9f09ea293e6318bcc2f4e020
warecuber <warecuber@gmail.com>
2021-02-17 14:20:28 -0600

    moved network configuration to new file

--- configure.sh
+++ configure.sh
@@ -1,33 +1,21 @@
-#!/bin/bash
-# echo "===== Starting configuration script ====="
-# sudo apt-get update
-# sudo apt-get upgrade -y
-# sudo apt-get install htop neofetch glances -y
-# echo "===== Creating SSH Group ====="
-# sudo groupadd sshuser
-# sudo usermod -aG sshuser $USER
-# echo "===== Configuring SSH ====="
-# sh ./ssh.sh
-# sudo ufw enable
-# mv ./update.sh ~/update.sh
-# echo "===== Adding custom shortcuts ====="
-# echo "alias update='sh ~/update.sh'" >> ~/.bashrc
-# echo "Added update command to shell"
+!/bin/bash
+echo "===== Starting configuration script ====="
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install htop neofetch glances -y
+echo "===== Creating SSH Group ====="
+sudo groupadd sshuser
+sudo usermod -aG sshuser $USER
+echo "===== Configuring SSH ====="
+sh ./ssh.sh
+sudo ufw enable
+mv ./update.sh ~/update.sh
+echo "===== Adding custom shortcuts ====="
+echo "alias update='sh ~/update.sh'" >> ~/.bashrc
+echo "Added update command to shell"
 read -n1 -p "Do you want to configure a static IP? y/n: " staticip
 if [[ $staticip == "y" || $staticip == "Y" ]]
 then
-    echo "\nconfiguring a static IP"
-    read -p "What shoud be the IP of the server? " serverip
-    read -p "What is the IP of the gateway? " gatewayip
-    read -p "Is the DNS the same as gateway? y/n: " isDNSSame
-    if [[ $isDNSSame == "n" || $isDNSSame == "N"]]
-    then
-        read -p "What DNS server should it use? " dnsIP
-    else
-        dnsIP=$gatewayip
-    fi
-    echo "IP: $serverip\nGateway: $gatewayip\nDNS Server: $dnsIP"
-else
-    echo "\nNot configuring static IP" 
+    sh ./staticip.sh
 fi
-echo "Done with IP"
+echo "===== Done! ====="
---
+++ staticip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "\nconfiguring a static IP"
+read -p "What shoud be the IP of the server? " serverip
+read -p "What is the IP of the gateway? " gatewayip
+read -n1 -p "Is the DNS the same as gateway? y/n: " isDNSSame
+if [[ $isDNSSame == "n" || $isDNSSame == "N" ]]
+then
+    read -p "What DNS server should it use? " dnsIP
+else
+    dnsIP=$gatewayip
+fi
+echo "network:"\
+"\n  ethernets:"\
+"\n    ens160:"\
+"\n      dhcp4: no"\
+"\n      addresses: [$serverip/24]"\
+"\n      gateway4: $gatewayip"\
+"\n      nameservers:"\
+"\n        addresses: [$dnsIP]"\
+"\n  version: 2" >> 00-installer-config.yaml
+sudo mv 00-installer-config.yaml /etc/netplan/00-installer-config.yaml
+sudo netplan --debug apply
