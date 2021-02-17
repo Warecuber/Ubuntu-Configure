591b722517edaa65d39123e18e88de8a6a1d9667
warecuber <warecuber@gmail.com>
2021-02-17 15:16:53 -0600

    Messed up ssh config file

--- ssh.sh
+++ ssh.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 sudo rm /etc/ssh/sshd_config
-echo "Include /etc/ssh/sshd_config.d/*.conf"\
-"\nPort 3300"\
-"\nLoginGraceTime 2m"\
-"\nPermitRootLogin prohibit-password"\
-"\nStrictModes yes"\
-"\nMaxAuthTries 2"\
-"\nMaxSessions 3"\
-"\nPubkeyAuthentication yes"\
-"\nAuthorizedKeysFile	.ssh/authorized_keys .ssh/authorized_keys2"\
-"\nPasswordAuthentication yes"\
-"\nPermitEmptyPasswords no"\
-"\nAllowGroups sshuser"\
-"\nChallengeResponseAuthentication no"\
-"\nUsePAM yes"\
-"\nX11Forwarding no"\
-"\nPrintMotd no"\
-"\nClientAliveInterval 60"\
-"\nClientAliveCountMax 3"\
-"\nAcceptEnv LANG LC_*"\
-"\nSubsystem sftp	/usr/lib/openssh/sftp-server" > sshd_config
+touch sshd_config
+echo "Include /etc/ssh/sshd_config.d/*.conf" >> sshd_config
+echo "Port 3300" >> sshd_config
+echo "LoginGraceTime 2m" >> sshd_config
+echo "PermitRootLogin prohibit-password" >> sshd_config
+echo "StrictModes yes" >> sshd_config
+echo "MaxAuthTries 2" >> sshd_config
+echo "MaxSessions 3" >> sshd_config
+echo "PubkeyAuthentication yes" >> sshd_config
+echo "AuthorizedKeysFile	.ssh/authorized_keys .ssh/authorized_keys2" >> sshd_config
+echo "PasswordAuthentication yes" >> sshd_config
+echo "PermitEmptyPasswords no" >> sshd_config
+echo "AllowGroups sshuser" >> sshd_config
+echo "ChallengeResponseAuthentication no" >> sshd_config
+echo "UsePAM yes" >> sshd_config
+echo "X11Forwarding no" >> sshd_config
+echo "PrintMotd no" >> sshd_config
+echo "ClientAliveInterval 60" >> sshd_config
+echo "ClientAliveCountMax 3" >> sshd_config
+echo "AcceptEnv LANG LC_*" >> sshd_config
+echo "Subsystem sftp	/usr/lib/openssh/sftp-server" >> sshd_config
 chmod 644 sshd_config
 sudo mv sshd_config /etc/ssh/sshd_config
 sudo service sshd restart
